5034c003eb0a68dbb5aac37ab4a83cca54c4f6bc
chrisssxuu <chris01.xu@gmail.com>
2018-08-08 18:15:08 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -20,10 +20,11 @@ end
 def add_colors
   # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
   rainbow_colors = [
-  "red",
-  "light_red",
-  "light_yellow"
+  "yellow",
+  "default",
+  "light_cyan"
 ]
   rainbow_colors << "green"
   rainbow_colors << "blue"
+  return rainbow_colors
 end
